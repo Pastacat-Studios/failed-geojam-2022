e816703b96a59563854ac6e9bc83f7a92bbf03c8
insert-usernamehere <montyd18@gmail.com>
2022-06-25 14:30:07 -0400

    saving and upgrades

---
+++ save_manager.gd
@@ -0,0 +1,22 @@
+extends Resource
+
+class_name upgrade_data
+
+export var upgrade_1_level = 0
+export var upgrade_2_level = 0
+export var upgrade_3_level = 0
+export var upgrade_4_level = 0
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
---
+++ upgrade_data.gd
@@ -0,0 +1,8 @@
+extends Resource
+
+class_name Upgrade_Data
+
+export var upgrade_1_level = 0
+export var upgrade_2_level = 0
+export var upgrade_3_level = 0
+export var upgrade_4_level = 0
